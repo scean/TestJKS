21fd34f710e3ab709fcd018da6ac2628fa0fe6d6
ruanjingchun <ruanjingchun@lancai.cn>
2017-12-04 14:59:48 +0800

    first commit

---
+++ TestIosGitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestIosGitHub
+//
+//  Created by ruanjingchun on 2017/12/4.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
